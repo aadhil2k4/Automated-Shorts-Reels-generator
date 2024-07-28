c4fc56cc69d723dbd300cb6eadad9ff1cb28da23
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-07-28 18:54:47 +0530

    prompt change

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#Youtube_Bot
+An Automated Youtube channel that posts "shorts" form of content daily to youtube.
+Action Plan:
+1. Fetch a real time trending topic in India.
+2. generate script using gemini
+3. use google image search api to download 4 images
+4. create a video using moviepy and text to voice api
+5. upload to youtube using selenium.
--- question.py
+++ question.py
@@ -11,7 +11,6 @@ genai.configure(api_key=os.getenv('GEMINI_KEY'))
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-response = model.generate_content(f"Generate a multiple-choice question about {most_trending} based on a well-established and verifiable fact. "
-          f"Provide 4 answer options where only one option is correct and clearly distinct from the incorrect options. "
-          f"At the end, mention the correct answer and provide a brief explanation supporting it.")
+response = model.generate_content(f"Generate a factual and interesting YouTube Short script about {most_trending}. "
+          f"The script should explain a unique, verifiable fact or story. Ensure the information is accurate and provide the source of the fact or story.")
 print(response.text)
