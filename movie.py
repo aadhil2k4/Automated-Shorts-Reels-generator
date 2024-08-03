e48927d0bc7b6d47bc89c1c9e8b2313063a3469f
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-08-03 18:29:30 +0530

    fix image and video resolution

--- movie.py
+++ movie.py
@@ -1,10 +1,8 @@
 import os
-from moviepy.editor import ImageClip, AudioFileClip, CompositeVideoClip, TextClip
+from moviepy.editor import *
 from gtts import gTTS
-from image import fetch_image
-from question import generate_question
-from trends import getMostTrending
 from dotenv import load_dotenv
+from PIL import Image
 
 load_dotenv()
 
@@ -13,15 +11,29 @@ def generate_audio(text, filename="narration.mp3"):
     tts.save(filename)
     return filename
 
+def resize_image(image_path, output_path, width, height):
+    with Image.open(image_path) as img:
+        img = img.resize((width, height), Image.LANCZOS)
+        if img.mode == 'RGBA':
+            img = img.convert('RGB')  
+        img.save(output_path, format='JPEG')
+
 def create_clip(image_path, text, output_path="short.mp4"):
     default_img_path = "default_img.jpg"
+    resized_img_path = "resized_img.jpg"
+    video_width, video_height = 1080, 1920
 
     if not image_path or not os.path.exists(image_path):
         image_path = default_img_path
 
+    resize_image(image_path, resized_img_path, video_width, video_height)
+
     audio_path = generate_audio(text)
-    image_clip = ImageClip(image_path)
     audio_clip = AudioFileClip(audio_path)
+    image_clip = ImageClip(resized_img_path)
+
     image_clip = image_clip.set_duration(audio_clip.duration)
     video = image_clip.set_audio(audio_clip)
     video.write_videofile(output_path, fps=24)
+
+
